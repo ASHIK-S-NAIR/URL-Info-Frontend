c6ef9bc9d04b128f7310d576873199229dd667c8
Ashik S Nair <aashiq5342@gmail.com>
2022-10-21 23:29:53 +0530

    working on home page section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <section className="home-section">
+      <div className="wrap home-wrap">
+        <h1 className="home-header">Supercharge your Content Marketing</h1>
+        <h3 className="home-subheader">We've built a simple growth marketing suite so you can improve <br /> website traffic with valuable insights and guidance.</h3>
+        <div className="home-url-sec">
+          <form className="home-url-form">
+            <input type="text" className="home-url-form-input" placeholder='Put your website URL here. Eg.www.growth.cx' />
+            <button className="btn-cta home-cta-getInsights">Get insights</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
