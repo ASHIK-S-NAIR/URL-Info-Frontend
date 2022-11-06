6758b67d35f3759f3907cb89227b9ac4df363ddf
Ashik S Nair <aashiq5342@gmail.com>
2022-11-06 22:19:29 +0530

    updated for deployment

--- src/setup/routes-manager/index.js
+++ src/setup/routes-manager/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "pages/components/Nav";
 import Home from "pages/Home";
@@ -7,9 +7,16 @@ import Signup from "pages/Auth/Signup";
 import PNF from "pages/PNF";
 import Footer from "pages/components/Footer";
 import { AuthContext } from "contexts/authContext";
+import { isAuthenticated } from "api/auth";
 
 const Router = () => {
   const [userAuthenticated, setUserAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setUserAuthenticated(true);
+    }
+  }, [userAuthenticated]);
   return (
     <AuthContext.Provider value={{ userAuthenticated, setUserAuthenticated }}>
       <BrowserRouter>
